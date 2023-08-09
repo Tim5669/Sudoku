a0a5eef46c660bbb8effb97adda493e4f054befe
Timothy <112266534+Tim5669@users.noreply.github.com>
2023-08-09 20:26:19 +0000

    Started making methods in the sudoku class

---
+++ Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "Sudoku.h"
+
+int main(){
+    return 0;
+}
---
+++ Sudoku.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+class Sudoku{
+    private:
+        int board[9][9];
+    public:
+        void boardSetUp(){
+            
+        }
+
+};
